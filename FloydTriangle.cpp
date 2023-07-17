191751e8b810f8791868416583a7135e08d4c56d
Rahul <rc7662837@gmail.com>
2023-07-17 12:31:00 +0530

    Initial commit

---
+++ FloydTriangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"How many lines do you want to print?"<<endl;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
